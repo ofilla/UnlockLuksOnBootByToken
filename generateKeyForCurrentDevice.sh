1a80410d339ccea1eb1c4e5a8f1659de49ec5388
Oliver Filla <ocl@hotmail.de>
2021-07-09 21:47:47 +0200

    Create function from script generateKeyForCurrentDevice.sh
    	* rename variable KEY_DEVICE_START to KEY_DEVICE_KEYFILE_STARTS_AT

--- generateKeyForCurrentDevice.sh
+++ generateKeyForCurrentDevice.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 # generates a decryption key for the current $KEY_DEVICE
 
-blocks_to_write=$(($KEY_DEVICE_LAST_WRITABLE - $KEY_DEVICE_FIRST_WRITABLE))
-dd if=/dev/urandom of=$KEY_DEVICE bs=$BLOCKSIZE seek=$KEY_DEVICE_FIRST_WRITABLE count=$blocks_to_write
+# go to directory containing this file
+cd "$(dirname "$BASH_SOURCE[0]")"
+# load config
+. decryptkeydevice.conf
 
-dd if=$KEY_DEVICE bs=$BLOCKSIZE skip=$KEY_DEVICE_START count=$BLOCK_NUMBER of=tempKeyFile.bin
+function generateKeyForCurrentDevice (
+    device=$1
 
-cryptsetup luksAddKey $CRYPTROOT_DEVICE tempKeyFile.bin
+    blocks_to_write=$(($KEY_DEVICE_LAST_WRITABLE - $KEY_DEVICE_FIRST_WRITABLE))
+    dd if=/dev/urandom of=$device bs=$BLOCKSIZE seek=$KEY_DEVICE_FIRST_WRITABLE count=$blocks_to_write
+
+    dd if=$device bs=$BLOCKSIZE skip=$KEY_DEVICE_KEYFILE_STARTS_AT count=$BLOCK_NUMBER of=tempKeyFile.bin
+
+    cryptsetup luksAddKey $CRYPTROOT_DEVICE tempKeyFile.bin
+
+    echo "Created keyfile on $device at $KEY_DEVICE_KEYFILE_STARTS_AT"
+    unset device
+)
+
+generateKeyForCurrentDevice $KEY_DEVICE
