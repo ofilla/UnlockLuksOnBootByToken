20bd145a55f3e5bfea18f96ce88e8e60012e6e12
Oliver Filla <ocl@hotmail.de>
2021-07-09 22:23:28 +0200

    Apply configuration to copied KEYSCRIPT and move local files

--- initAutoUnlockOnBootConfig.sh
+++ initAutoUnlockOnBootConfig.sh
@@ -10,17 +10,21 @@ function initAutoUnlockOnBootConfig_using_initramfs() {
     mkdir /etc/decryptkeydevice
 
     # update device ids in in this file
-    cp decryptkeydevice.sh $KEYSCRIPT
-    chmod +x $KEYSCRIPT
+    cp files/decryptkeydevice.sh $KEYSCRIPT
+    sed -i "s:\$KEY_DEVICE_IDS:$KEY_DEVICE_IDS:" $KEYSCRIPT
+    sed -i "s:\$BLOCKSIZE:$BLOCKSIZE:" $KEYSCRIPT
+    sed -i "s:\$KEY_DEVICE_KEYFILE_STARTS_AT:$KEY_DEVICE_KEYFILE_STARTS_AT:" $KEYSCRIPT
+    sed -i "s:\$BLOCK_NUMBER:$BLOCK_NUMBER:" $KEYSCRIPT
+    chmod 700 $KEYSCRIPT
 
     echo "source=$CRYPTROOT_DEVICE,target=$ROOT_NAME,lvm=$LVM_ROOT,keyscript=$KEYSCRIPT,key=$KEY_DEVICE" > /etc/initramfs-tools/conf.d/cryptroot
     chmod +x /etc/initramfs-tools/conf.d/cryptroot
 
-    cp decryptkeydevice.hook /etc/initramfs-tools/hooks/decryptkeydevice.hook
+    cp files/decryptkeydevice.initramfs.hook /etc/initramfs-tools/hooks/decryptkeydevice.hook
     chmod +x /etc/initramfs-tools/hooks/decryptkeydevice.hook
 
     # update blockids in this file
-    cat modules >> /etc/initramfs-tools/modules
+    cat files/modules >> /etc/initramfs-tools/modules
 
     update-initramfs -u
 }
