af67ce0364fe91b4565373ebf74b1b5bdaf6d7c6
Oliver Filla <ocl@hotmail.de>
2021-07-09 21:52:08 +0200

    Create function from script initAutoUnlockOnBootConfig.sh

--- initAutoUnlockOnBootConfig.sh
+++ initAutoUnlockOnBootConfig.sh
@@ -1,19 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-# copy new config
-mkdir /etc/decryptkeydevice
+# go to directory containing this file
+cd "$(dirname "$BASH_SOURCE[0]")"
+# load config
+. decryptkeydevice.conf
 
-# update device ids in in this file
-cp decryptkeydevice.sh $KEYSCRIPT
-chmod +x $KEYSCRIPT
+function initAutoUnlockOnBootConfig_using_initramfs() {
+    # copy new config
+    mkdir /etc/decryptkeydevice
 
-echo "source=$CRYPTROOT_DEVICE,target=$ROOT_NAME,lvm=$LVM_ROOT,keyscript=$KEYSCRIPT,key=$KEY_DEVICE" > /etc/initramfs-tools/conf.d/cryptroot
-chmod +x /etc/initramfs-tools/conf.d/cryptroot
+    # update device ids in in this file
+    cp decryptkeydevice.sh $KEYSCRIPT
+    chmod +x $KEYSCRIPT
 
-cp decryptkeydevice.hook /etc/initramfs-tools/hooks/decryptkeydevice.hook
-chmod +x /etc/initramfs-tools/hooks/decryptkeydevice.hook
+    echo "source=$CRYPTROOT_DEVICE,target=$ROOT_NAME,lvm=$LVM_ROOT,keyscript=$KEYSCRIPT,key=$KEY_DEVICE" > /etc/initramfs-tools/conf.d/cryptroot
+    chmod +x /etc/initramfs-tools/conf.d/cryptroot
 
-# update blockids in this file
-cat modules >> /etc/initramfs-tools/modules
+    cp decryptkeydevice.hook /etc/initramfs-tools/hooks/decryptkeydevice.hook
+    chmod +x /etc/initramfs-tools/hooks/decryptkeydevice.hook
 
-update-initramfs -u
+    # update blockids in this file
+    cat modules >> /etc/initramfs-tools/modules
+
+    update-initramfs -u
+}
+
+initAutoUnlockOnBootConfig_using_initramfs
