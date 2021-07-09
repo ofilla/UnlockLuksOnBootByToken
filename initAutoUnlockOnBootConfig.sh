be2130fde31f6a003182f36f18a69f936723150d
Oliver Filla <ocl@hotmail.de>
2021-07-09 22:30:58 +0200

    Extract function configure_keyscript in initAutoUnlockOnBootConfig.sh

--- initAutoUnlockOnBootConfig.sh
+++ initAutoUnlockOnBootConfig.sh
@@ -5,17 +5,23 @@ cd "$(dirname "$BASH_SOURCE[0]")"
 # load config
 . decryptkeydevice.conf
 
+function configure_keyscript() {
+    keyscript="$1"
+
+    sed -i "s:\$KEY_DEVICE_IDS:$KEY_DEVICE_IDS:" $keyscript
+    sed -i "s:\$BLOCKSIZE:$BLOCKSIZE:" $keyscript
+    sed -i "s:\$KEY_DEVICE_KEYFILE_STARTS_AT:$KEY_DEVICE_KEYFILE_STARTS_AT:" $keyscript
+    sed -i "s:\$BLOCK_NUMBER:$BLOCK_NUMBER:" $keyscript
+    chmod 700 $keyscript
+}
+
 function initAutoUnlockOnBootConfig_using_initramfs() {
     # copy new config
     mkdir /etc/decryptkeydevice
 
     # update device ids in in this file
     cp files/decryptkeydevice.sh $KEYSCRIPT
-    sed -i "s:\$KEY_DEVICE_IDS:$KEY_DEVICE_IDS:" $KEYSCRIPT
-    sed -i "s:\$BLOCKSIZE:$BLOCKSIZE:" $KEYSCRIPT
-    sed -i "s:\$KEY_DEVICE_KEYFILE_STARTS_AT:$KEY_DEVICE_KEYFILE_STARTS_AT:" $KEYSCRIPT
-    sed -i "s:\$BLOCK_NUMBER:$BLOCK_NUMBER:" $KEYSCRIPT
-    chmod 700 $KEYSCRIPT
+    configure_keyscript $KEYSCRIPT
 
     echo "source=$CRYPTROOT_DEVICE,target=$ROOT_NAME,lvm=$LVM_ROOT,keyscript=$KEYSCRIPT,key=$KEY_DEVICE" > /etc/initramfs-tools/conf.d/cryptroot
     chmod +x /etc/initramfs-tools/conf.d/cryptroot
