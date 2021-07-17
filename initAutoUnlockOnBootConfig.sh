a6f359c35b20d7bbbd00e669a45b4c22a3ca51a6
Oliver Filla <ocl@hotmail.de>
2021-07-17 21:16:22 +0200

    Improve installer to install correctly
    Choose system (initramfs or initcpio) by existing folders in /etc

--- initAutoUnlockOnBootConfig.sh
+++ initAutoUnlockOnBootConfig.sh
@@ -35,4 +35,38 @@ function initAutoUnlockOnBootConfig_using_initramfs() {
     update-initramfs -u
 }
 
-initAutoUnlockOnBootConfig_using_initramfs
+function initAutoUnlockOnBootConfig_using_initcpio() {
+    cp files/decryptkeydevice.initcpio.hook /etc/initcpio/hooks/decryptkeydevice
+    cp files/decryptkeydevice.initcpio.install /etc/initcpio/install/decryptkeydevice
+
+    cat > /etc/decryptkeydevice.conf <<EOF
+# configuration for decryptkeydevice
+#
+
+# ID(s) of the USB/MMC key(s) for decryption (sparated by blanks)
+# as listed in /dev/disk/by-id/
+DECRYPTKEYDEVICE_DISKID="$KEY_DEVICE_IDS"
+
+# blocksize usually 512 is OK
+DECRYPTKEYDEVICE_BLOCKSIZE="$BLOCKSIZE"
+
+# start of key information on keydevice DECRYPTKEYDEVICE_BLOCKSIZE * DECRYPTKEYDEVICE_SKIPBLOCKS
+DECRYPTKEYDEVICE_SKIPBLOCKS="$KEY_DEVICE_KEYFILE_STARTS_AT"
+
+# length of key information on keydevice DECRYPTKEYDEVICE_BLOCKSIZE * DECRYPTKEYDEVICE_READBLOCKS
+DECRYPTKEYDEVICE_READBLOCKS="$BLOCK_NUMBER"
+EOF
+
+    chmod 600 /etc/decryptkeydevice.conf
+    chmod 600 /etc/initcpio/hooks/decryptkeydevice
+    chmod 600 /etc/initcpio/install/decryptkeydevice
+
+    sed -i '/^HOOKS=/s/encrypt/decryptkeydevice encrypt/' /etc/mkinitcpio.conf
+    mkinitcpio -P
+}
+
+if [[ -d /etc/initramfs-tools ]]; then
+    initAutoUnlockOnBootConfig_using_initramfs
+elif [[ -d /etc/initcpio ]]; then
+    initAutoUnlockOnBootConfig_using_initcpio
+fi
